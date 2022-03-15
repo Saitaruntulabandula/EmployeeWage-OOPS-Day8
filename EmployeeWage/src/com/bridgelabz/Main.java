08c2dc2526c6ef1931d9b73b3b2919cef8e58e8e
Saitaruntulabandula <saitaruntulabandula@gmail.com>
2022-03-15 10:04:44 +0530

    Added

--- EmployeeWage/src/com/bridgelabz/Main.java
+++ EmployeeWage/src/com/bridgelabz/Main.java
@@ -1,9 +1,16 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Employee Wages....\n");
-		EmployeeWage.checkAttendance();
+		System.out.println("Enter \n  Employee Wage per hour \n  Employee Hours");
+		Scanner scanner = new Scanner(System.in);
+		int wagePerHour = scanner.nextInt();
+		int empHrs = scanner.nextInt();
+		EmployeeWage.dailyWage(wagePerHour,empHrs);
+		scanner.close();
 	}
 }
