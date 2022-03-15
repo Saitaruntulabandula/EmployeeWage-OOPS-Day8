a66dbe32540a559b4bb95bd67b2735eb874e6cc0
Saitaruntulabandula <saitaruntulabandula@gmail.com>
2022-03-15 10:42:04 +0530

    Added

--- EmployeeWage/src/com/bridgelabz/EmployeeWage.java
+++ EmployeeWage/src/com/bridgelabz/EmployeeWage.java
@@ -3,16 +3,23 @@ package com.bridgelabz;
 public class EmployeeWage {
 	static final int IS_FULL_TIME=1;
 	static final int IS_PART_TIME=2;
+
 	static void dailyWage(int wagePerHour, int fullTimeEmpHrs, int partTimeEmpHrs) {
 		int value = (int)Math.floor(Math.random()*10) % 3;
 		int empWage=0;
-		if(value == IS_FULL_TIME) {
+		switch(value) {
+		case IS_FULL_TIME:{
 			empWage = wagePerHour * fullTimeEmpHrs; 
 			System.out.println("Employee is Present Full Day and Wage is : " +empWage);
-		}else if(value == IS_PART_TIME) {
-			empWage = wagePerHour * partTimeEmpHrs; 
-			System.out.println("Employee is Present Half Day and Wage is : " +empWage);
+			break;
 		}
-		else System.out.println("Employee is Absent today, as the reason today's wage is : "+empWage);
+		case IS_PART_TIME:{
+			empWage = wagePerHour * partTimeEmpHrs;
+			System.out.println("Employee is Present Half Day and Wage is : " +empWage);
+			break;
+		}
+		default: 
+			System.out.println("Employee is Absent today, as the reason today's wage is : "+empWage);
+		}
 	}
 }
