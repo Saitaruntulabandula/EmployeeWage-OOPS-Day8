fec14c321365d9dab5b1cad972b180cfac0e7d3c
Saitaruntulabandula <saitaruntulabandula@gmail.com>
2022-03-18 22:49:19 +0530

    Added

---
+++ EmployeeWage/src/com/bridgelabz/CompanyEmpWage.java
@@ -0,0 +1,28 @@
+package com.bridgelabz;
+
+public class CompanyEmpWage {
+	String companyName;
+	int wagePerHr;
+	int maxWorkingDays;
+	int maxWorkingHrs;
+	int totalWage;
+
+	CompanyEmpWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs)
+	{
+		this.companyName = companyName;
+		this.wagePerHr = wagePerHr;
+		this.maxWorkingDays = maxWorkingDays;
+		this.maxWorkingHrs = maxWorkingHrs;
+		totalWage = 0;
+	}
+
+	void setTotalEmployeeWage(int totalEmpWage)
+	{
+		this.totalWage = totalEmpWage;
+	}
+
+	@Override
+	public String toString() {
+		return "CompanyEmpWage [companyName = " + companyName + ", totalWage = " + totalWage + "]";
+	}
+}
